47c864dcbaa57f055a9734ddcf35739cf48516fc
Lao Tong <lao.s.t@pm.me>
2022-08-09 13:40:47 -0600

    update orbchan project settings

--- orbchan/urls.py
+++ orbchan/urls.py
@@ -1,17 +1,17 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from boards import views as board_views
+from orbs.views import home, boards, board, create_thread
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("orbs.urls", namespace="orbs")),
-    path("", include("boards.urls", namespace="boards")),
+    path("", home, name="home"),
+    path("<slug:board_slug>/", board, name="board"),
+    path("<slug:board_slug>/create_thread/", create_thread, name="create_thread"),
 ]
 
-if settings.DEBUG == True:
-    urlpatterns += [path("__reload__/", include("django_browser_reload.urls"))]
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
